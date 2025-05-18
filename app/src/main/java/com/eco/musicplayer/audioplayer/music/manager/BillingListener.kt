39c45ae990b8023f2e2ce69670b6d04b10d7e5e8
ADMIN <v93920923@gmail.com>
2025-05-18 09:47:08 +0700

    -tinhnv update

--- app/src/main/java/com/eco/musicplayer/audioplayer/music/manager/BillingListener.kt
+++
@@ -1,9 +0,0 @@
-package com.eco.musicplayer.audioplayer.music.manager
-
-import com.android.billingclient.api.Purchase
-
-interface BillingListener {
-    fun onPurchaseSuccess(purchase: Purchase)
-    fun onPurchaseError(errorCode: Int, errorMessage: String)
-    fun onPurchaseCancelled()
-}
--- app/src/main/java/com/eco/musicplayer/audioplayer/music/state/PaywallUiState.kt
+++
@@ -1,6 +0,0 @@
-package com.eco.musicplayer.audioplayer.music.state
-
-sealed class PaywallUiState {
-    data class PurchaseSuccess(val productId: String) : PaywallUiState()
-    data class Error(val message: String) : PaywallUiState()
-}
